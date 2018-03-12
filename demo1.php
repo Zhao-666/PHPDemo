3e2fee7a28d18d5c02ab7835215b5aa1392cb94c
Zhao-666 <2268699370@qq.com>
2018-03-12 14:13:32 +0800

    test

--- demo1.php
+++ demo1.php
@@ -6,14 +6,19 @@
  * Time: 16:36
  */
 
-$a = range(0,1000);
-var_dump(memory_get_usage());
-//COW机制 Copy On Write
-$b = $a;
-var_dump(memory_get_usage());
+//$a = range(0,1000);
+//var_dump(memory_get_usage());
+////COW机制 Copy On Write
+//$b = $a;
+//var_dump(memory_get_usage());
+//
+//$a = range(0,1000);
+//var_dump(memory_get_usage());
+//
+//$a =  '1qwe' == 1;
+//var_dump($a);
 
-$a = range(0,1000);
-var_dump(memory_get_usage());
-
-$a =  '1qwe' == 1;
-var_dump($a);
+$file = fopen('hello.txt','w');
+$str = phpinfo();
+fwrite($file,$str);
+fclose($file);
